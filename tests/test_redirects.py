04c72041f2a19b70658190b20850668827d11fba
Glenn Howard <glennrphoward@gmail.com>
2012-10-11 21:33:42 +0100

    adding as  firefox-latest-euballot must be in a european locale

--- tests/test_redirects.py
+++ tests/test_redirects.py
@@ -70,16 +70,17 @@ class TestRedirects(Base):
         Assert.equal(parsed_url.netloc, 'download-installer.cdn.mozilla.net', 'Failed on %s \nUsing %s' % (url, param))
 
     @pytest.mark.parametrize('product_alias', [
-        {'product_name': 'firefox-aurora-latest'},
-        {'product_name': 'firefox-beta-latest'},
-        {'product_name': 'firefox-latest-euballot'},
-        {'product_name': 'firefox-latest'},
-        {'product_name': 'firefox-nightly-latest'}])
+        {'product_name': 'firefox-aurora-latest', 'lang': 'en-US'},
+        {'product_name': 'firefox-beta-latest', 'lang': 'en-US'},
+        {'product_name': 'firefox-latest-euballot', 'lang': 'en-GB'},
+        {'product_name': 'firefox-latest', 'lang': 'en-US'},
+        {'product_name': 'firefox-nightly-latest', 'lang': 'en-US'}])
     def test_redirect_for_firefox_aliases(self, testsetup, product_alias):
         url = testsetup.base_url
         param = {
             'product': product_alias['product_name'],
-            'os': 'win'
+            'os': 'win',
+            'lang': product_alias['lang']
         }
 
         response = self._head_request(url, params=param)
